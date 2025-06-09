a762049f8cbf4a640b5d9f6ccf86d5c629badcc4
DolapoSalaam <dolaposalaam@outlook.com>
2025-06-09 22:18:58 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+function calcBmi(weight, height){
+    return (weight / (height/100) ** 2);
+}
+function receiveValues(event){
+    event.preventDefault();
+
+    let weight = parseFloat(document.getElementById('weight').value);
+
+    let height = parseFloat(document.getElementById('height').value);
+
+    let resultDiv = document.getElementById('bmi-result')
+
+    if ((weight <= 0)|| (height <= 0)){
+        resultDiv.innerHTML = 'please enter valid positive numbers'
+    } else {
+        let bmi = calcBmi(weight,height)
+        resultDiv.innerHTML = `your BMI is: ${bmi.toFixed(2)}`
+    }
+
+}
+
+document.querySelector('form').addEventListener('submit', receiveValues)
+    
